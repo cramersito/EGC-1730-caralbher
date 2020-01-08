ff74d2a0274f078e9c204f478230e73743aac7a7
carletes <carlosalbalat97@gmail.com>
2020-01-08 12:15:44 +0000

    2

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -151,6 +151,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
